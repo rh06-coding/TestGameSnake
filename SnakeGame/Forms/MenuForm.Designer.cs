fd4ea02dbd37044adf822920531fadd16b65c598
lthnoname <henryle987654@gmail.com>
2025-12-04 13:08:03 +0700

    tạo radio button chọn background game và button start game

--- SnakeGame/Forms/MenuForm.Designer.cs
+++ SnakeGame/Forms/MenuForm.Designer.cs
@@ -29,20 +29,22 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.BlueSnakeRad = new System.Windows.Forms.RadioButton();
+            this.RedSnakeRad = new System.Windows.Forms.RadioButton();
+            this.DefaultSnakeRad = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.Background1Rad = new System.Windows.Forms.RadioButton();
+            this.Background2Rad = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton4);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.BlueSnakeRad);
+            this.groupBox1.Controls.Add(this.RedSnakeRad);
+            this.groupBox1.Controls.Add(this.DefaultSnakeRad);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(776, 151);
@@ -50,31 +52,43 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Hãy chọn tạo hình rắn";
             // 
-            // radioButton2
+            // BlueSnakeRad
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Image = global::SnakeGame.Properties.Resources.RedHead;
-            this.radioButton2.Location = new System.Drawing.Point(297, 23);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(145, 128);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.BlueSnakeRad.AutoSize = true;
+            this.BlueSnakeRad.Image = global::SnakeGame.Properties.Resources.BlueHead;
+            this.BlueSnakeRad.Location = new System.Drawing.Point(534, 23);
+            this.BlueSnakeRad.Name = "BlueSnakeRad";
+            this.BlueSnakeRad.Size = new System.Drawing.Size(145, 128);
+            this.BlueSnakeRad.TabIndex = 2;
+            this.BlueSnakeRad.TabStop = true;
+            this.BlueSnakeRad.UseVisualStyleBackColor = true;
             // 
-            // radioButton1
+            // RedSnakeRad
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Image = global::SnakeGame.Properties.Resources.DefaultSnakeHead;
-            this.radioButton1.Location = new System.Drawing.Point(82, 23);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(145, 128);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.RedSnakeRad.AutoSize = true;
+            this.RedSnakeRad.Image = global::SnakeGame.Properties.Resources.RedHead;
+            this.RedSnakeRad.Location = new System.Drawing.Point(311, 23);
+            this.RedSnakeRad.Name = "RedSnakeRad";
+            this.RedSnakeRad.Size = new System.Drawing.Size(145, 128);
+            this.RedSnakeRad.TabIndex = 1;
+            this.RedSnakeRad.TabStop = true;
+            this.RedSnakeRad.UseVisualStyleBackColor = true;
+            // 
+            // DefaultSnakeRad
+            // 
+            this.DefaultSnakeRad.AutoSize = true;
+            this.DefaultSnakeRad.Image = global::SnakeGame.Properties.Resources.DefaultSnakeHead;
+            this.DefaultSnakeRad.Location = new System.Drawing.Point(82, 23);
+            this.DefaultSnakeRad.Name = "DefaultSnakeRad";
+            this.DefaultSnakeRad.Size = new System.Drawing.Size(145, 128);
+            this.DefaultSnakeRad.TabIndex = 0;
+            this.DefaultSnakeRad.TabStop = true;
+            this.DefaultSnakeRad.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.Background2Rad);
+            this.groupBox2.Controls.Add(this.Background1Rad);
             this.groupBox2.Location = new System.Drawing.Point(12, 193);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(776, 151);
@@ -82,33 +96,48 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Hãy chọn Background";
             // 
-            // radioButton3
+            // Background1Rad
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(82, 61);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(82, 20);
-            this.radioButton3.TabIndex = 0;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Mặc định";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.Background1Rad.AutoSize = true;
+            this.Background1Rad.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Background1Rad.Location = new System.Drawing.Point(82, 61);
+            this.Background1Rad.Name = "Background1Rad";
+            this.Background1Rad.Size = new System.Drawing.Size(107, 20);
+            this.Background1Rad.TabIndex = 0;
+            this.Background1Rad.TabStop = true;
+            this.Background1Rad.Text = "Backgound 1";
+            this.Background1Rad.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // Background2Rad
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Image = global::SnakeGame.Properties.Resources.BlueHead;
-            this.radioButton4.Location = new System.Drawing.Point(534, 23);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(145, 128);
-            this.radioButton4.TabIndex = 2;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.Background2Rad.AutoSize = true;
+            this.Background2Rad.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Background2Rad.Location = new System.Drawing.Point(534, 61);
+            this.Background2Rad.Name = "Background2Rad";
+            this.Background2Rad.Size = new System.Drawing.Size(107, 20);
+            this.Background2Rad.TabIndex = 1;
+            this.Background2Rad.TabStop = true;
+            this.Background2Rad.Text = "Backgound 2";
+            this.Background2Rad.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(323, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "MenuForm";
@@ -124,10 +153,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton RedSnakeRad;
+        private System.Windows.Forms.RadioButton DefaultSnakeRad;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton Background1Rad;
+        private System.Windows.Forms.RadioButton BlueSnakeRad;
+        private System.Windows.Forms.RadioButton Background2Rad;
+        private System.Windows.Forms.Button button1;
     }
 }
