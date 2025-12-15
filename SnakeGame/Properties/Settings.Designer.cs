6d79e2bd25b4ba83b81d5f344a019f5103682e18
Lê Hoàng <hoanglevt2006@gmail.com>
2025-12-15 13:30:29 +0700

    Update Setting

--- SnakeGame/Properties/Settings.Designer.cs
+++ SnakeGame/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace SnakeGame.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|QuanLyNguoiChoi" +
-            ".mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|QuanLyNguoiCho" +
+            "i.mdf;Integrated Security=True;Connect Timeout=30")]
         public string QuanLyNguoiChoiConnectionString {
             get {
                 return ((string)(this["QuanLyNguoiChoiConnectionString"]));
